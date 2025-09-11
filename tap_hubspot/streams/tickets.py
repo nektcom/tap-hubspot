ddad88235316b8a0079965dbb1002c79c74b8f51
Pedro Ceriotti <pedro@nekt.ai>
2025-09-10 22:56:11 -0300

    feat: Add support to ticket associations

--- tap_hubspot/streams/tickets.py
+++ tap_hubspot/streams/tickets.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from functools import cached_property
+
 from nekt_singer_sdk import typing as th  # JSON Schema typing helpers
 from tap_hubspot.client import DynamicIncrementalHubspotStream
 
@@ -25,9 +27,56 @@ class TicketStream(DynamicIncrementalHubspotStream):
     replication_key = "hs_lastmodifieddate"
     records_jsonpath = "$[results][*]"  # Or override `parse_response`.
 
+    @cached_property
+    def associations_string_list(self) -> list[str]:
+        return self.config.get("extract_ticket_associations_comma_separated_string").replace(" ", "").split(",")
+
+    @cached_property
+    def should_extract_associations(self) -> bool:
+        return self.config.get("extract_ticket_associations") and self.associations_string_list
+
+    @cached_property
+    def schema(self) -> dict:
+        schema = super().schema
+        if self.should_extract_associations:
+            associations_schema = th.PropertiesList()
+            for association_string in self.associations_string_list:
+                associations_schema.append(
+                    th.Property(
+                        association_string,
+                        th.ArrayType(
+                            th.ObjectType(
+                                th.Property("id", th.StringType),
+                                th.Property("type", th.StringType),
+                            )
+                        ),
+                    )
+                )
+
+            schema["properties"]["associations"] = associations_schema.to_dict()
+        return schema
+
+    def post_process(self, row, context=None):
+        if not (self.should_extract_associations):
+            return super().post_process(row, context)
+
+        additional_data = self._fetch_additional_data_with_retry(
+            "tickets",
+            row["id"],
+            self.associations_string_list if self.should_extract_associations else None,
+        )
+
+        if "associations" in additional_data:
+            row["associations"] = additional_data["associations"]
+        if "propertiesWithHistory" in additional_data:
+            row["propertiesWithHistory"] = additional_data["propertiesWithHistory"]
+
+        return super().post_process(row, context)
+
     @property
     def url_base(self) -> str:
         """
         Returns an updated path which includes the api version
         """
+        return "https://api.hubapi.com/crm/v3"
         return "https://api.hubapi.com/crm/v3"
--- tap_hubspot/tap.py
+++ tap_hubspot/tap.py
@@ -99,6 +99,20 @@ class TapHubspot(Tap):
             description="Comma separated string with the name of entities that should be extracted",
         ),
         th.Property(
+            "extract_ticket_associations",
+            th.BooleanType,
+            default=False,
+            required=True,
+            description="Enable the extraction of entities associated with tickets",
+        ),
+        th.Property(
+            "extract_ticket_associations_comma_separated_string",
+            th.StringType,
+            required=False,
+            default="calls, communications, companies, contacts, emails, meetings, notes, subscriptions, tasks",
+            description="Comma separated string with the name of entities that should be extracted",
+        ),
+        th.Property(
             "extract_contact_property_history",
             th.BooleanType,
             default=False,
