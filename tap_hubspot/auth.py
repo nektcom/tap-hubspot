6a19ae1e149c97814fab57c7b809339491f98b32
Pedro Ceriotti <pedro@nekt.ai>
2025-10-09 11:41:05 -0300

    fix: Handle token expiration

--- tap_hubspot/auth.py
+++ tap_hubspot/auth.py
@@ -1,6 +1,8 @@
 """HubSpot Authentication."""
 
 from nekt_singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
+from nekt_singer_sdk.helpers._util import utc_now
+from typing_extensions import override
 
 
 class HubSpotOAuthAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
@@ -14,3 +16,13 @@ class HubSpotOAuthAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
             "client_secret": self.config["oauth_credentials"]["client_secret"],
             "refresh_token": self.config["oauth_credentials"]["refresh_token"],
         }
+
+    @override
+    def is_token_valid(self) -> bool:
+        if self.last_refreshed is None:
+            return False
+        if not self.expires_in:
+            return True
+        return (self.expires_in - 60) > (
+            utc_now() - self.last_refreshed
+        ).total_seconds()  # anticipate 60 seconds to avoid race condition
